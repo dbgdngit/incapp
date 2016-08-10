99bc4df8b7434fe04a53b0957c6765fa4eb3aa58
dbgdngit <dave.boxall@guardian.co.uk>
2016-08-10 10:55:15 +0100

    added social auth domain whitelist

--- incapp/incapp/settings.py
+++ incapp/incapp/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
 ]
 SITE_ID = 1
-
+GOOGLE_WHITE_LISTED_DOMAINS = ['guardian.co.uk']  
 LOGIN_REDIRECT_URL = '/'
 SOCIALACCOUNT_QUERY_EMAIL = True
 SOCIALACCOUNT_PROVIDERS = {
